efa8533cea03686aa589c94a65b0d32ed3210bc8
hamishtodd1 <s1560142@SCE-BIO-C04326.bio.ed.ac.uk>
2016-01-11 18:29:38 +0000

    rearrangement

---
+++ Data/internet stuff dummy.js
@@ -0,0 +1,21 @@
+var secondsthroughvid = animation_beginning_second;
+
+function react_to_video(){
+	secondsthroughvid += delta_t;
+}
+
+function load_dummy_textures(){
+	var mywidth = 3;
+	for(var i = 0; i < picture_objects.length; i++) {
+		picture_objects[i] = new THREE.Mesh(new THREE.CubeGeometry( mywidth,mywidth,0), 
+											new THREE.MeshBasicMaterial({color:0x000ff0}) );
+
+		picture_objects[i].position.x = -playing_field_width / 2;
+		picture_objects[i].position.z = 0.01;
+	}
+	
+}
+
+load_dummy_textures();
+init();
+render();
